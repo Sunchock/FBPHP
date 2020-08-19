75f8654612dcddea256d35cc4b9b1842a0891be1
Sunchock <toto.marestin@gmail.com>
2020-08-20 00:03:01 +0200

    Add todo

--- index.php
+++ index.php
@@ -2,12 +2,34 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $fb = new Facebook\Facebook([
-	'app_id' => '{app-id}',
-	'app_secret' => '{app-secret}',
+	'app_id' => '597810607548604',
+	'app_secret' => 'a8c09aaf47211cdfa3821c294958cea6',
 	'default_graph_version' => 'v2.10',
 ]);
 
-var_dump($fb);exit;
+/* PHP SDK v5.0.0 */
+/* TODO
+1 - Get UserID
+2 - Get PageID
+3 - Get Page access_token (!= user access_token !!!)
+4 - Feed Page with his own access_token and message
+*/
+/* make the API call */
+/*try {
+	// Returns a `Facebook\FacebookResponse` object
+	$response = $fb->get(
+	  '/{page-id}_{post-id}/',
+	  '{access-token}'
+	);
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+	echo 'Graph returned an error: ' . $e->getMessage();
+	exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+	echo 'Facebook SDK returned an error: ' . $e->getMessage();
+	exit;
+}
+$graphNode = $response->getGraphNode();*/
+/* handle the result */
 ?>
 <!DOCTYPE>
 <html>
